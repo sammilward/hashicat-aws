ca8315d6878e3a75a7557f7c02793a9c2979b2b3
Sam Milward <32576769+sammilward@users.noreply.github.com>
2022-01-20 11:21:34 +0000

    Update template v2
    Update template v2

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. This code change has gone through a PR via GitHub.
+  Welcome to ${PREFIX}'s app. This code change has gone through a PR via GitHub. V2
   <!-- END -->
 
   </div>
